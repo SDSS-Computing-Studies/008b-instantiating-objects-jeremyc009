6652bef25b8215d394eea2e0f3357307e73dd84f
jeremyc009 <jeremyc009@deltalearns.ca>
2020-10-29 12:16:11 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,5 +1,79 @@
 #!python3
 
+"""
+Create a class that will store a database for a veterinarian.
+Your class will need the following properties:
+
+animal (dog, cat, fish, bird, turtle, etc)
+breed
+name (the pet's name)
+owner (the owner's name)
+birthdate (of the pet, expressed as yyyy-mm-dd)
+
+The constructor will need to ask for each of those values
+for the database when the pet is instantiated
+
+Methods:
+display() - should show the name of the pet and type followed by their breed and owner
+
+
+Main block should have a menu that has the following options:
+1. Enter a new pet
+2. Retrieve a pet
+3. Exit
+
+If they choose to retrieve a pet,
+display the following:
+Enter pet's name:
+and then go through the list, looking for the name of the pet.
+If the pet is found, it should call the display() method from the object
+
+Example output:
+1. Enter a new pet
+2. Retrieve a pet
+3. Exit
+1
+
+Type of animal? cat
+Breed? Domestic Long Hair
+Name? Benjamin
+Owner? Chris
+Birthdate? 20015-10-01
+
+1. Enter a new pet
+2. Retrieve a pet
+3. Exit
+1
+
+Type of animal? dog
+Breed? Shih-tzu
+Name? Buster
+Owner? Christy
+Birthdate? 2008-10-16
+
+1. Enter a new pet
+2. Retrieve a pet
+3. Exit
+1
+
+Type of animal? cat
+Breed? Domestic Long Hair
+Name? Casey
+Owner? Chris
+Birthdate? 20015-10-01
+
+1. Enter a new pet
+2. Retrieve a pet
+3. Exit
+2
+
+Which Pet? Buster
+
+Buster dog
+Shih-tzu is owned by Christy
+(10 points) 
+"""
+
 class animal:
 
     typ=None
@@ -9,11 +83,11 @@ class animal:
     birthdate=None
 
     def __init__(self):
-        self.typ=input()
-        self.breed=input()
-        self.name=input( )
-        self.owner=input()
-        self.birthdate=(input())
+        self.typ=input("Enter the Type of animal: ")
+        self.breed=input("Enter the breed of animal: ")
+        self.name=input('Enter the name of the animal: ' )
+        self.owner=input('Enter the name of the owner: ')
+        self.birthdate=(input('Enter the birthdate: '))
     
     def display(self):
         print(self.name)
@@ -25,13 +99,15 @@ def main():
     animals=[]
     num = 0
     while True:
-       
-        num=input()
-        num = int(num)
+        print("Please select one of the following options: ")
+        print("1: Enter a new pet")
+        print('2: Retrieve a pet')
+        print('3: Exit')
+        num=int(input())
         if num == 1:
             animals.append(animal())
         if num == 2:
-            ind=input().strip()
+            ind=input('Enter the name of the animal: ')
             length=len(animals)
             for i in range(0,length):
                 name=animals[i].name
@@ -43,9 +119,8 @@ def main():
 
             
         if num == 3:
-            print()
+            print('Thank you for using this program.')
             break
             
 
 main()
-
